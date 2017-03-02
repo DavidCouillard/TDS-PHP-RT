b6b82c4f16cbdeb548c79cf916d7e3837bb15d4e
David COUILLARD <David COUILLARD>
2017-03-02 18:29:20 +0100

    fin

---
+++ TestSite/operation.php
@@ -0,0 +1,12 @@
+<form>
+	<input type="number" name = "b">
+	<input type="number" name = "b">
+	<input type="submit" value="Ajouter">
+</form>
+
+<?php
+if(isset($_GET["a"]) && isset($_GET["b"])){
+	$resultat=$_GET["a"]+$_GET["b"];
+	echo "La somme est $resultat";
+}
+?>
